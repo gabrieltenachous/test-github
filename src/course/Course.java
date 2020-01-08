42f4f2294264eeb9c6f191cdb380744a532b8fb1
gabrieltenachous <gabriel_oliveira_f_sousa@outlook.com>
2020-01-08 14:56:01 -0300

    boa tarde boa noite

--- src/course/Course.java
+++ src/course/Course.java
@@ -3,5 +3,7 @@ package course;
 public class Course{
     public static void main(String[] args) {
         System.out.println("bom dia");
+        System.out.println("boa tarde");
+        System.out.println("boa noite");
     }
 }
